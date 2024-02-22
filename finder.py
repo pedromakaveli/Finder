d6d553d970ee019588be4459a27733fb9830e0ad
pedromakaveli <pedrolacerda804@gmail.com>
2024-02-22 17:06:26 -0300

    Add new variables on Objeto class

--- finder.py
+++ finder.py
@@ -5,7 +5,13 @@ from time import sleep
 init()
 
 class Objeto:
-    ...
+    #-----------------------------------------------------------------------------
+    # This variables are passed by a instance locate on the end of this code
+    currentExtension = ''
+    currentPath = os.getcwd()
+    filesPath = []
+    #-----------------------------------------------------------------------------
+
 # Here we find the main menu
 
 class Main:
